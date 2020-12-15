ef5ae2d46e84c2d024e9dac490299f3f7e7c98c6
everimind4 <everimind4@gmail.com>
2020-12-15 20:27:32 +0900

    Update MazeRunner.c
    타이틀과 메뉴 그리는 코드를 함수로 분리

--- MazeRunner/MazeRunner.c
+++ MazeRunner/MazeRunner.c
@@ -2,39 +2,17 @@
 #include <stdlib.h>
 #include <Windows.h>
 
-#define ALIGN "                                                   "
-
 void go(int, int);
+void DrawTitle();
+void DrawMenu();
 
 int main()
 {
     system("cls | color 3E"); // Set Console Color
     SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), CONSOLE_FULLSCREEN_MODE, 0); // Set Fullscreen Mode
-    int row = 54, col = 10;
 
-    // Title :: 메이즈러너
-    go(row, col++);  printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"); puts("");
-    go(row, col);    printf("■"); go(row + 118, col++); printf("■"); puts("");
-    go(row, col);    printf("■"); go(row + 118, col++); printf("■"); puts("");
-
-    go(row, col);    printf("■"); go(row + 12, col++);  printf("■■■■  ■  ■      ■■■    ■    ■■■■■■■    ■■■■    ■    ■        ■ ■■■■■■■■   ■"); puts("");
-    go(row, col);    printf("■"); go(row + 11, col++);  printf("■    ■  ■  ■    ■      ■  ■          ■                ■    ■    ■        ■                     ■"); puts("");
-    go(row, col);    printf("■"); go(row + 10, col++);  printf("■    ■  ■  ■    ■      ■  ■        ■  ■              ■    ■    ■        ■ ■■■■■■■       ■"); puts("");
-    go(row, col);    printf("■"); go(row + 9, col++);   printf("■    ■■■  ■    ■      ■  ■      ■      ■      ■■■■  ■■    ■    ■■■                       ■"); puts("");
-    go(row, col);    printf("■"); go(row + 8, col++);   printf("■    ■  ■  ■    ■      ■  ■    ■          ■    ■          ■    ■        ■ ■■■■■■           ■"); puts("");
-    go(row, col);    printf("■"); go(row + 7, col++);   printf("■    ■  ■  ■    ■      ■  ■                      ■          ■    ■        ■                         ■"); puts("");
-    go(row, col);    printf("■"); go(row + 6, col++);   printf("■■■■  ■  ■      ■■■    ■    ■■■■■■■    ■■■■    ■    ■■■■  ■ ■■■■■               ■"); puts("");
-    
-    go(row, col);    printf("■"); go(row + 118, col++); printf("■"); puts("");
-    go(row, col);    printf("■"); go(row + 118, col++); printf("■"); puts("");
-    go(row, col);    printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"); puts("");
-
-    // Menu
-    col = 35;
-    go(row + 30, col);
-
-    printf("test");
-
+    DrawTitle();
+    DrawMenu();
 
 }
 
@@ -42,4 +20,39 @@ void go(int x, int y)
 {
     COORD point = { x - 1, y - 1 };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), point);
+}
+
+void DrawTitle() {
+    int row = 50, col = 10;
+    go(row, col++);  printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+    go(row, col);    printf("■"); go(row + 122, col++); printf("■");
+    go(row, col);    printf("■"); go(row + 122, col++); printf("■");
+
+    go(row, col);    printf("■"); go(row + 12, col++);  printf("■■■■  ■  ■      ■■■    ■    ■■■■■■■        ■■■■    ■    ■        ■ ■■■■■■■■   ■");
+    go(row, col);    printf("■"); go(row + 11, col++);  printf("■    ■  ■  ■    ■      ■  ■          ■                    ■    ■    ■        ■                     ■");
+    go(row, col);    printf("■"); go(row + 10, col++);  printf("■    ■  ■  ■    ■      ■  ■        ■  ■                  ■    ■    ■        ■ ■■■■■■■       ■");
+    go(row, col);    printf("■"); go(row + 9, col++);   printf("■    ■■■  ■    ■      ■  ■      ■      ■          ■■■■  ■■    ■    ■■■                       ■");
+    go(row, col);    printf("■"); go(row + 8, col++);   printf("■    ■  ■  ■    ■      ■  ■    ■          ■        ■          ■    ■        ■ ■■■■■■           ■");
+    go(row, col);    printf("■"); go(row + 7, col++);   printf("■    ■  ■  ■    ■      ■  ■                          ■          ■    ■        ■                         ■");
+    go(row, col);    printf("■"); go(row + 6, col++);   printf("■■■■  ■  ■      ■■■    ■    ■■■■■■■        ■■■■    ■    ■■■■  ■ ■■■■■               ■");
+
+    go(row, col);    printf("■"); go(row + 122, col++); printf("■");
+    go(row, col);    printf("■"); go(row + 122, col++); printf("■");
+    go(row, col);    printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+}
+
+void DrawMenu() {
+    // Menu
+    int row = 90, col = 40;
+    go(row, col++); printf("■■■■  ■■■    ■■    ■■■    ■■■");
+    go(row, col++); printf("■          ■    ■    ■  ■    ■    ■");
+    go(row, col++); printf("■■■■    ■    ■■■■  ■■■      ■");
+    go(row, col++); printf("      ■    ■    ■    ■  ■    ■    ■");
+    go(row, col++); printf("■■■■    ■    ■    ■  ■    ■    ■");
+    row += 8; col += 3;
+    go(row, col++); printf("■■■■  ■  ■  ■  ■■■");
+    go(row, col++); printf("■         ■■   ■    ■");
+    go(row, col++); printf("■■■■    ■    ■    ■");
+    go(row, col++); printf("■         ■■   ■    ■");
+    go(row, col++); printf("■■■■  ■  ■  ■    ■");
 }
